b47a348590efebd95283a5d0df00d1e162a57e34
Bruno Oliveira <boliveirasilva@gmail.com>
2015-09-11 19:36:17 -0300

    - Configuração do Ambiente. - Acrescenta o Composer com o PHPUnit como dependencia. - Acrescenta phpunit.xml e public/bootstrap.php - Acrescenta primeira classe e os primeiros testes

---
+++ src/PhoneTools/PhoneTools.php
@@ -0,0 +1,12 @@
+<?php 
+namespace PhoneTools;
+
+class PhoneTools
+{
+    private $telefones;
+
+    public function __construct()
+    {
+        $this->telefones = null;
+    }
+}
---
+++ tests/PhoneTools/PhoneToolsTest.php
@@ -0,0 +1,22 @@
+<?php 
+namespace Tryout\PhoneTools;
+
+use PhoneTools\PhoneTools;
+
+class ClassName extends \PHPUnit_Framework_TestCase
+{
+
+    public function assertPreConditions()
+    {
+        $this->assertTrue(
+            class_exists($class = 'PhoneTools\PhoneTools'), 
+            'Classe não encontrada: ' . $class
+        );
+    }
+
+    public function testInstantiationWithoutArgumentsShouldWork()
+    {
+        $instance = new PhoneTools();
+        $this->assertInstanceOf('PhoneTools\PhoneTools', $instance);
+    }
+}
